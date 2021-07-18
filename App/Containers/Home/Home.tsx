021a817438b837ad4e49323c478a316bcfcd6b6e
Jonathan Taveras <jonathanrtv@outlook.com>
2021-07-18 14:14:07 -0400

    Implement screen: home

--- App/Containers/Home/Home.tsx
+++ App/Containers/Home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { ScaledSheet } from 'react-native-size-matters';
+import { Theme, i18n } from '../../Core';
 import { Button, Text } from '../../Components';
 
 interface Props {
@@ -10,28 +12,55 @@ interface Props {
 const Home = (props: Props) => {
   const { onBeginPress } = props;
   return (
-    <View style={styles.container}>
-      <Text label={'hello'} size={'largeTitle'} />
-      <Button
-        onPress={onBeginPress}
-        label={'Press Me!'}
-        textProps={{
-          size: 'title2',
-        }}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style={'light'} />
+      <View style={styles.content}>
+        <View style={styles.topEmptyContent} />
+        <View style={styles.infoContent}>
+          <Text
+            label={i18n.t('Home.WelcomeToTriviaChallenge')}
+            size={'largeTitle'}
+            textAlignment={'center'}
+            medium={true}
+          />
+
+          <Text
+            label={i18n.t('Home.questionInfo')}
+            size={'title2'}
+            textAlignment={'center'}
+          />
+          <Text
+            label={i18n.t('Home.questionScore')}
+            size={'title3'}
+            textAlignment={'center'}
+          />
+          <Button
+            label={i18n.t('Home.begin')}
+            onPress={onBeginPress}
+            textProps={{ medium: true }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = ScaledSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Theme.colors.primary,
   },
-  textRegular: {
-    fontFamily: 'Poppins_400Regular',
+  topEmptyContent: {
+    flex: 1,
+  },
+  infoContent: {
+    justifyContent: 'space-around',
+    flex: 2,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    ...Theme.spacers.MA_5,
   },
 });
 
